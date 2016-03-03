9bff9a0ce7dc08985c9d4348fd007a72290fd435
Jungtae JI <yamnjjt@hotmail.com>
2016-03-04 02:23:46 +1000

    initial commit

---
+++ src/AngularChecklist/scripts/app.component.ts
@@ -0,0 +1,10 @@
+﻿import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: './app/app.component.html'
+})
+
+export class AppComponent {
+    title = 'this is first angular app that is genuinely done by myself';
+}
---
+++ src/AngularChecklist/scripts/main.ts
@@ -0,0 +1,5 @@
+﻿import {bootstrap} from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
+
+// adding the component to the app component
+bootstrap(AppComponent);
